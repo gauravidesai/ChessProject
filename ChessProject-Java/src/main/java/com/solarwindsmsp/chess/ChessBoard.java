0029a7b5719c056e66dd0509de07a9b0547131f7
gauravidesai <86884363+gauravidesai@users.noreply.github.com>
2021-07-26 12:00:41 +0100

    Add files via upload

--- ChessProject-Java/src/main/java/com/solarwindsmsp/chess/ChessBoard.java
+++ ChessProject-Java/src/main/java/com/solarwindsmsp/chess/ChessBoard.java
@@ -11,11 +11,43 @@ public class ChessBoard {
         pieces = new Pawn[MAX_BOARD_WIDTH][MAX_BOARD_HEIGHT];
     }
 
-    public void addPiece(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.add()");
+
+    public void addPiece(Pawn pawn, int xCoordinate, int yCoordinate, PieceColor pieceColor) throws InvalidPositionException {
+
+        if ( !isLegalBoardPosition( xCoordinate, yCoordinate ) )
+        {
+            throw new InvalidPositionException( "Position: " + xCoordinate + "," + yCoordinate + " outwith board limits", this, xCoordinate, yCoordinate);
+        }
+        pawn.setXCoordinate( xCoordinate );
+        pawn.setYCoordinate( yCoordinate );
+        pawn.setChessBoard( this );
+        this.pieces[xCoordinate][yCoordinate] = pawn;
+
     }
 
-    public boolean isLegalBoardPosition(int xCoordinate, int yCoordinate) {
-        throw new UnsupportedOperationException("Need to implement ChessBoard.IsLegalBoardPosition()");
+    public boolean isLegalBoardPosition( int xCoordinate, int yCoordinate ) throws InvalidPositionException
+    {
+        if ( xCoordinate < 0 || xCoordinate >= MAX_BOARD_WIDTH || yCoordinate < 0 || yCoordinate >= MAX_BOARD_HEIGHT )
+        {
+            //log.warn( "Coordinate: {},{} is outwith this board's limits", xCoordinate, yCoordinate );
+            System.out.println("Coordinate is outwith this board's limits");
+            return false;
+        }
+
+        if ( null != getPieceAtCoordinate( xCoordinate, yCoordinate ) )
+        {
+            //log.warn( "Coordinate: {},{} is already populated", xCoordinate, yCoordinate );
+            System.out.println("Coordinate is already populated");
+            return false;
+        }
+
+        return true;
     }
+
+    public Pawn getPieceAtCoordinate( int xCoordinate, int yCoordinate )
+    {
+        return this.pieces[xCoordinate][yCoordinate];
+    }
+
+
 }
--- ChessProject-Java/src/main/java/com/solarwindsmsp/chess/Pawn.java
+++ ChessProject-Java/src/main/java/com/solarwindsmsp/chess/Pawn.java
@@ -3,39 +3,66 @@ package com.solarwindsmsp.chess;
 public class Pawn {
 
     private ChessBoard chessBoard;
-    private int xCoordinate;
-    private int yCoordinate;
-    private PieceColor pieceColor;
+    private int xCoordinate = -1;
+    private int yCoordinate = -1;
+    private int initXCoordinate = -1;
+    private int initYCoordinate = -1;
+    protected PieceColor pieceColor;
+    private Pawn capturedBy;
 
     public Pawn(PieceColor pieceColor) {
         this.pieceColor = pieceColor;
     }
-
-    public ChessBoard getChessBoard() {
+    public ChessBoard getChessBoard()
+    {
         return chessBoard;
     }
 
-    public void setChessBoard(ChessBoard chessBoard) {
+    public void setChessBoard( ChessBoard chessBoard )
+    {
         this.chessBoard = chessBoard;
     }
 
-    public int getXCoordinate() {
+    public int getXCoordinate()
+    {
         return xCoordinate;
     }
 
-    public void setXCoordinate(int value) {
+    public void setXCoordinate( int value )
+    {
         this.xCoordinate = value;
+        if ( this.initXCoordinate == -1 && this.xCoordinate > -1 )
+        {
+            this.initXCoordinate = this.xCoordinate;
+        }
     }
 
-    public int getYCoordinate() {
+    public int getYCoordinate()
+    {
         return yCoordinate;
     }
 
-    public void setYCoordinate(int value) {
+    public void setYCoordinate( int value )
+    {
         this.yCoordinate = value;
+        if ( this.initYCoordinate == -1 && this.yCoordinate > -1 )
+        {
+            this.initYCoordinate = this.yCoordinate;
+        }
     }
 
-    public PieceColor getPieceColor() {
+    public int getInitXCoordinate()
+    {
+        return initXCoordinate;
+    }
+
+    public int getInitYCoordinate()
+    {
+        return initYCoordinate;
+    }
+
+    public PieceColor getPieceColor()
+    {
         return this.pieceColor;
     }
 
@@ -43,8 +70,83 @@ public class Pawn {
         pieceColor = value;
     }
 
-    public void move(MovementType movementType, int newX, int newY) {
-        throw new UnsupportedOperationException("Need to implement Pawn.Move()") ;
+    public Pawn getCapturedBy()
+    {
+        return capturedBy;
+    }
+
+    public void capturePiece( Pawn capturedPiece )
+    {
+        capturedPiece.capturedBy = this;
+        capturedPiece.setXCoordinate( -1 );
+        capturedPiece.setYCoordinate( -1 );
+    }
+
+    public void move(MovementType movementType, int newX, int newY) throws InvalidMovementException
+    {
+
+        switch ( movementType )
+        {
+            case MOVE:
+                if ( !isValidMoveForChessPiece( newX, newY ) )
+                {
+                    throw new InvalidMovementException( "Invalid movement", this, newX, newY, movementType );
+                }
+                break;
+            case CAPTURE:
+                if ( !isValidCaptureForChessPiece( newX, newY ) )
+                {
+                    throw new InvalidMovementException( "Invalid movement", this, newX, newY, movementType );
+                }
+                break;
+            default:
+                throw new InvalidMovementException( "Unrecognised MovementType: " + movementType, this, newX, newY, movementType );
+        }
+
+
+        // does a piece already occupy the new spot?
+        Pawn occupyingPiece = getChessBoard().getPieceAtCoordinate( newX, newY );
+
+        // if valid position and a piece already exists that isn't on the same side, capture
+        if ( occupyingPiece != null )
+        {
+            // same colour, same side
+            if ( this.getPieceColor().equals( occupyingPiece.getPieceColor() ) )
+            {
+                throw new InvalidMovementException( "A Chess piece on the same side already exists at position " + newX + "," + newY, this, newX, newY, movementType );
+            }
+
+            this.capturePiece( occupyingPiece );
+        }
+
+        // now move the piece to the new position
+        this.setXCoordinate( newX );
+        this.setYCoordinate( newY );
+
+
+    }
+
+    public boolean isValidMoveForChessPiece( int xCoordinate, int yCoordinate )
+    {
+        // Pawns can only move one space forward
+        if ( this.getInitYCoordinate() - 2 < 0 )
+        {
+            // this pawn started at the bottom the of board so y must be one greater than current y coordinate
+            return ( yCoordinate - this.getYCoordinate() == 1 );
+        }
+        else
+        {
+            // this pawn started at the top the of board so y must be one less than current y coordinate
+            return ( yCoordinate - this.getYCoordinate() ) == -1;
+
+        }
+
+
+    }
+
+    public boolean isValidCaptureForChessPiece( int xCoordinate, int yCoordinate )
+    {
+        throw new UnsupportedOperationException( "Need to implement Pawn.isValidCaptureForChessPiece()" );
     }
 
     @Override
